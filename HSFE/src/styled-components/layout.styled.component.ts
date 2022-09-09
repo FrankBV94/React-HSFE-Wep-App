3b5d6fdfa615b69c598101ef4ea0036d896e775a
FrankBV94 <frankbotelle@gmail.com>
2022-09-09 10:44:40 -0400

    Estilos

--- HSFE/src/styled-components/layout.styled.component.ts
+++ HSFE/src/styled-components/layout.styled.component.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-
+/**
+ * Define un "div" con determinados estilos
+ */
 export const LayoutContainer = styled.div`
-  @media only screen and (min-width: 1421px) {
-    width: 100%;
-  }
+  width: 80vw;
+
   @media only screen and (max-width: 1176px) {
     padding: 12px;
   }
