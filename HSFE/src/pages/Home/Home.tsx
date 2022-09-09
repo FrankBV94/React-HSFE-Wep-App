1f0aa464a9fd218dc256ef1ac1c855aa6603f0c5
FrankBV94 <frankbotelle@gmail.com>
2022-09-09 10:32:12 -0400

    Llenado de tablada con datos de people.ts

--- HSFE/src/pages/Home/Home.tsx
+++ HSFE/src/pages/Home/Home.tsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data/people";
-/**
- * TODO: Terminar tabla
- */
+
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
   const pageSize = 5;
   const colums = [
-    {}
-  ]
+    {
+      field: "name",
+      headerName: "Nombre",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categoria",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Compañia",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "levelOfHappiness",
+      headerName: "Nivel de felicidad",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
   return (
-    <div>
       <DataGrid
         disableColumnSelector
         disableSelectionOnClick
@@ -20,8 +41,9 @@ const Home: React.FC<HomeInterface> = () => {
         pageSize={pageSize}
         rowsPerPageOptions={[pageSize]}
         rows={People}
+        columns={colums}
+        getRowId={(row: any) => row.id}
       />
-    </div>
   );
 };
 
