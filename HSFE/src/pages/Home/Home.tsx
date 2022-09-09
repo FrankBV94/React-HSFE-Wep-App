225ca96f958c5bbb32fc33c7f302d50b4d3a66dc
FrankBV94 <frankbotelle@gmail.com>
2022-09-08 22:05:52 -0400

    TODO: Terminar tabla

--- HSFE/src/pages/Home/Home.tsx
+++ HSFE/src/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { DataGrid } from "@mui/x-data-grid";
+import { People } from "@/data/people";
+/**
+ * TODO: Terminar tabla
+ */
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-  return <div>Home</div>;
+  const pageSize = 5;
+  const colums = [
+    {}
+  ]
+  return (
+    <div>
+      <DataGrid
+        disableColumnSelector
+        disableSelectionOnClick
+        autoHeight
+        pageSize={pageSize}
+        rowsPerPageOptions={[pageSize]}
+        rows={People}
+      />
+    </div>
+  );
 };
 
 export default Home;
